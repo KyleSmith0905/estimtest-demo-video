558895e22d7399ccea917bf8cc88b51cfb82eb49
Kyle Smith <kylesmith090502@gmail.com>
2023-03-13 13:35:10 -0400

    Create new Remotion video

---
+++ remotion.config.ts
@@ -0,0 +1,7 @@
+import {Config} from 'remotion';
+
+Config.setImageFormat('png');
+Config.setPixelFormat('yuva444p10le');
+Config.setCodec('prores');
+Config.setProResProfile('4444');
+Config.setMuted(true);
